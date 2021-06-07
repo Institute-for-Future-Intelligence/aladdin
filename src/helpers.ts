f69b1930734696fce4a742b41c09f87d5bd73665
Charles Xie <charxie@gmail.com>
2021-06-07 18:03:51 -0400

    map image on the ground using static API

--- src/helpers.ts
+++ src/helpers.ts
@@ -8,6 +8,11 @@ export const visitIFI = () => {
     window.open("https://intofuture.org", '_blank');
 };
 
+export const getMapImage = (size: number, lat: number, lng: number, zoom: number) => {
+    return "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=" + lat + "," + lng
+        + "&zoom=" + zoom + "&size=" + size + "x" + size + "&scale=2&key=" + process.env.REACT_APP_MAPS_API_KEY;
+}
+
 export const showSuccess = (msg: string) => {
     message.success({
         content: msg,
