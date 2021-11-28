b8d3071b89d33cdf107489489c4ef488c5da67ca
Charles Xie <charxie@gmail.com>
2021-11-28 17:43:10 -0500

    update scene radius when opening a file

--- src/elementsRenderer.tsx
+++ src/elementsRenderer.tsx
@@ -34,39 +34,9 @@ const ElementsRenderer: React.FC<ElementsRendererProps> = ({}: ElementsRendererP
 
   useEffect(() => {
     if (heliodon) {
-      if (groupRef.current) {
-        const boxes = [];
-        for (const group of groupRef.current.children) {
-          const children = group.children.filter((x) => x.userData['aabb']);
-          for (const c of children) {
-            boxes.push(new Box3().setFromObject(c));
-          }
-        }
-        if (boxes.length > 0) {
-          const min = new Vector3();
-          const max = new Vector3();
-          for (const box of boxes) {
-            min.min(box.min);
-            max.max(box.max);
-          }
-
-          let r = Math.abs(min.x);
-          if (r < Math.abs(min.y)) r = Math.abs(min.y);
-          if (r < Math.abs(min.z)) r = Math.abs(min.z);
-          if (r < Math.abs(max.x)) r = Math.abs(max.x);
-          if (r < Math.abs(max.y)) r = Math.abs(max.y);
-          if (r < Math.abs(max.z)) r = Math.abs(max.z);
-          setCommonStore((state) => {
-            state.aabb = new Box3(min, max);
-            if (!isNaN(r) && isFinite(r)) {
-              // have to round this, otherwise the result is different even if nothing moved.
-              state.sceneRadius = Math.round(Math.max(10, r * 1.25)); // make it 25% larger than the bounding box
-            }
-          });
-        }
-      }
       setCommonStore((state) => {
         state.viewState.showHeliodonAfterBoundingBox = true;
+        state.updateSceneRadiusFlag = !state.updateSceneRadiusFlag;
       });
     } else {
       setCommonStore((state) => {
