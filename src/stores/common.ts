67576cd0b11ee08f8e7095d69496d3ae47f68bf5
Xiaotong <xd59@njit.edu>
2021-11-28 11:45:55 -0500

    revert heliodon work

--- src/stores/common.ts
+++ src/stores/common.ts
@@ -289,8 +289,8 @@ export interface CommonStoreState {
   cameraDirection: Vector3;
   getCameraDirection: () => Vector3;
 
-  heliodonRadius: number;
-  setHeliodonRadius: (radius: number) => void;
+  sceneRadius: number;
+  setSceneRadius: (radius: number) => void;
 
   selectedElementAngle: number;
   selectedElementHeight: number;
@@ -2105,10 +2105,10 @@ export const useStore = create<CommonStoreState>(
             return get().cameraDirection;
           },
 
-          heliodonRadius: 10,
-          setHeliodonRadius(radius: number) {
+          sceneRadius: 10,
+          setSceneRadius(radius: number) {
             immerSet((state: CommonStoreState) => {
-              state.heliodonRadius = radius;
+              state.sceneRadius = radius;
             });
           },
 
