d5f6d94b2ea44f5b4d79a2f43648fc48f92555ec
Charles Xie <charxie@gmail.com>
2021-12-09 19:51:19 -0500

    keep children's absolute positions when resizing of selected parents is undone/redone

--- src/components/contextMenu/elementMenu/foundationWidthInput.tsx
+++ src/components/contextMenu/elementMenu/foundationWidthInput.tsx
@@ -9,9 +9,11 @@ import { useStore } from '../../../stores/common';
 import * as Selector from '../../../stores/selector';
 import { ObjectType, Scope } from '../../../types';
 import i18n from '../../../i18n/i18n';
-import { UndoableChange } from '../../../undo/UndoableChange';
-import { UndoableChangeGroup } from '../../../undo/UndoableChangeGroup';
 import { FoundationModel } from '../../../models/FoundationModel';
+import { Vector2 } from 'three';
+import { Util } from '../../../Util';
+import { UndoableSizeGroupChange } from '../../../undo/UndoableSizeGroupChange';
+import { UndoableSizeChange } from '../../../undo/UndoableSizeChange';
 
 const FoundationWidthInput = ({
   widthDialogVisible,
@@ -22,9 +24,12 @@ const FoundationWidthInput = ({
 }) => {
   const language = useStore(Selector.language);
   const elements = useStore(Selector.elements);
+  const updateElementCxById = useStore(Selector.updateElementCxById);
   const updateElementLxById = useStore(Selector.updateElementLxById);
   const updateElementLxForAll = useStore(Selector.updateElementLxForAll);
   const getSelectedElement = useStore(Selector.getSelectedElement);
+  const getChildren = useStore(Selector.getChildren);
+  const setElementPosition = useStore(Selector.setElementPosition);
   const addUndoable = useStore(Selector.addUndoable);
   const foundationActionScope = useStore(Selector.foundationActionScope);
   const setFoundationActionScope = useStore(Selector.setFoundationActionScope);
@@ -34,7 +39,13 @@ const FoundationWidthInput = ({
   const [updateFlag, setUpdateFlag] = useState<boolean>(false);
   const [dragEnabled, setDragEnabled] = useState<boolean>(false);
   const [bounds, setBounds] = useState<DraggableBounds>({ left: 0, top: 0, bottom: 0, right: 0 } as DraggableBounds);
+
+  const oldChildrenPositionsMapRef = useRef<Map<string, Vector2>>(new Map<string, Vector2>());
+  const newChildrenPositionsMapRef = useRef<Map<string, Vector2>>(new Map<string, Vector2>());
+  const unnormalizedPosMapRef = useRef<Map<string, Vector2>>(new Map());
   const dragRef = useRef<HTMLDivElement | null>(null);
+  const rejectRef = useRef<boolean>(false);
+  const rejectedValue = useRef<number | undefined>();
 
   const lang = { lng: language };
 
@@ -49,50 +60,156 @@ const FoundationWidthInput = ({
     setUpdateFlag(!updateFlag);
   };
 
-  const setLx = (value: number) => {
+  const containsAllChildren = (lx: number) => {
     switch (foundationActionScope) {
       case Scope.AllObjectsOfThisType:
-        const oldLxsAll = new Map<string, number>();
-        for (const elem of elements) {
-          if (elem.type === ObjectType.Foundation) {
-            oldLxsAll.set(elem.id, elem.lx);
+        for (const e of elements) {
+          if (e.type === ObjectType.Foundation) {
+            const f = e as FoundationModel;
+            const children = getChildren(f.id);
+            if (children.length > 0) {
+              if (!Util.doesNewSizeContainAllChildren(f, children, lx, f.ly)) {
+                return false;
+              }
+            }
           }
         }
-        const undoableChangeAll = {
-          name: 'Set Width for All Foundations',
-          timestamp: Date.now(),
-          oldValues: oldLxsAll,
-          newValue: value,
-          undo: () => {
-            for (const [id, lx] of undoableChangeAll.oldValues.entries()) {
-              updateElementLxById(id, lx as number);
-            }
-          },
-          redo: () => {
-            updateElementLxForAll(ObjectType.Foundation, undoableChangeAll.newValue as number);
-          },
-        } as UndoableChangeGroup;
-        addUndoable(undoableChangeAll);
-        updateElementLxForAll(ObjectType.Foundation, value);
         break;
       default:
-        if (foundation) {
-          const oldLx = foundation.lx;
+        const children = getChildren(foundation.id);
+        if (children.length > 0) {
+          return Util.doesNewSizeContainAllChildren(foundation, children, lx, foundation.ly);
+        }
+    }
+    return true;
+  };
+
+  const rejectChange = (lx: number) => {
+    // check if the new width will still contain all children of the foundations in the selected scope
+    if (!containsAllChildren(lx)) {
+      return true;
+    }
+    // other check?
+    return false;
+  };
+
+  const updateLxWithChildren = (parent: FoundationModel, value: number) => {
+    // store children's relative positions
+    const children = getChildren(parent.id);
+    const origin = new Vector2(0, 0);
+    const azimuth = parent.rotation[2];
+    unnormalizedPosMapRef.current.clear(); // this map is for one-time use with each foundation
+    if (children.length > 0) {
+      for (const c of children) {
+        switch (c.type) {
+          case ObjectType.Wall:
+            break;
+          case ObjectType.SolarPanel:
+          case ObjectType.Sensor:
+            const p = new Vector2(c.cx * parent.lx, c.cy * parent.ly).rotateAround(origin, azimuth);
+            unnormalizedPosMapRef.current.set(c.id, p);
+            oldChildrenPositionsMapRef.current.set(c.id, new Vector2(c.cx, c.cy));
+            break;
+        }
+      }
+    }
+    // update foundation's length
+    updateElementLxById(parent.id, value);
+    // update children's relative positions
+    if (children.length > 0) {
+      for (const c of children) {
+        const p = unnormalizedPosMapRef.current.get(c.id);
+        if (p) {
+          const relativePos = new Vector2(p.x, p.y).rotateAround(origin, -azimuth);
+          const newCx = relativePos.x / value;
+          updateElementCxById(c.id, newCx);
+          newChildrenPositionsMapRef.current.set(c.id, new Vector2(newCx, c.cy));
+        }
+      }
+    }
+  };
+
+  const setLx = (value: number) => {
+    const oldLx = foundation.lx;
+    rejectedValue.current = undefined;
+    rejectRef.current = rejectChange(value);
+    if (rejectRef.current) {
+      rejectedValue.current = value;
+      setInputLx(oldLx);
+    } else {
+      oldChildrenPositionsMapRef.current.clear();
+      newChildrenPositionsMapRef.current.clear();
+      switch (foundationActionScope) {
+        case Scope.AllObjectsOfThisType:
+          const oldLxsAll = new Map<string, number>();
+          for (const elem of elements) {
+            if (elem.type === ObjectType.Foundation) {
+              oldLxsAll.set(elem.id, elem.lx);
+            }
+          }
+          // the following also populates the above two maps in ref
+          for (const elem of elements) {
+            if (elem.type === ObjectType.Foundation) {
+              updateLxWithChildren(elem as FoundationModel, value);
+            }
+          }
+          const undoableChangeAll = {
+            name: 'Set Width for All Foundations',
+            timestamp: Date.now(),
+            oldSizes: oldLxsAll,
+            newSize: value,
+            oldChildrenPositionsMap: new Map(oldChildrenPositionsMapRef.current),
+            newChildrenPositionsMap: new Map(newChildrenPositionsMapRef.current),
+            undo: () => {
+              for (const [id, lx] of undoableChangeAll.oldSizes.entries()) {
+                updateElementLxById(id, lx as number);
+              }
+              if (undoableChangeAll.oldChildrenPositionsMap.size > 0) {
+                for (const [id, p] of undoableChangeAll.oldChildrenPositionsMap.entries()) {
+                  setElementPosition(id, p.x, p.y);
+                }
+              }
+            },
+            redo: () => {
+              updateElementLxForAll(ObjectType.Foundation, undoableChangeAll.newSize as number);
+              if (undoableChangeAll.newChildrenPositionsMap.size > 0) {
+                for (const [id, p] of undoableChangeAll.newChildrenPositionsMap.entries()) {
+                  setElementPosition(id, p.x, p.y);
+                }
+              }
+            },
+          } as UndoableSizeGroupChange;
+          addUndoable(undoableChangeAll);
+          break;
+        default:
+          updateLxWithChildren(foundation, value);
           const undoableChange = {
             name: 'Set Foundation Width',
             timestamp: Date.now(),
-            oldValue: oldLx,
-            newValue: value,
+            oldSize: oldLx,
+            newSize: value,
+            resizedElementId: foundation.id,
+            oldChildrenPositionsMap: new Map(oldChildrenPositionsMapRef.current),
+            newChildrenPositionsMap: new Map(newChildrenPositionsMapRef.current),
             undo: () => {
-              updateElementLxById(foundation.id, undoableChange.oldValue as number);
+              updateElementLxById(foundation.id, undoableChange.oldSize as number);
+              if (undoableChange.oldChildrenPositionsMap.size > 0) {
+                for (const [id, p] of undoableChange.oldChildrenPositionsMap.entries()) {
+                  setElementPosition(id, p.x, p.y);
+                }
+              }
             },
             redo: () => {
-              updateElementLxById(foundation.id, undoableChange.newValue as number);
+              updateElementLxById(foundation.id, undoableChange.newSize as number);
+              if (undoableChange.newChildrenPositionsMap.size > 0) {
+                for (const [id, p] of undoableChange.newChildrenPositionsMap.entries()) {
+                  setElementPosition(id, p.x, p.y);
+                }
+              }
             },
-          } as UndoableChange;
+          } as UndoableSizeChange;
           addUndoable(undoableChange);
-          updateElementLxById(foundation.id, value);
-        }
+      }
     }
     setUpdateFlag(!updateFlag);
   };
@@ -122,6 +239,13 @@ const FoundationWidthInput = ({
             onMouseOut={() => setDragEnabled(false)}
           >
             {i18n.t('word.Width', lang)}
+            <label style={{ color: 'red', fontWeight: 'bold' }}>
+              {rejectRef.current
+                ? ': ' +
+                  i18n.t('shared.NotApplicableToSelectedAction', lang) +
+                  (rejectedValue.current !== undefined ? ' (' + rejectedValue.current.toFixed(2) + ')' : '')
+                : ''}
+            </label>
           </div>
         }
         footer={[
@@ -137,6 +261,7 @@ const FoundationWidthInput = ({
             key="Cancel"
             onClick={() => {
               setInputLx(foundation?.lx);
+              rejectRef.current = false;
               setWidthDialogVisible(false);
             }}
           >
@@ -147,7 +272,9 @@ const FoundationWidthInput = ({
             type="primary"
             onClick={() => {
               setLx(inputLx);
-              setWidthDialogVisible(false);
+              if (!rejectRef.current) {
+                setWidthDialogVisible(false);
+              }
             }}
           >
             {i18n.t('word.OK', lang)}
@@ -156,6 +283,7 @@ const FoundationWidthInput = ({
         // this must be specified for the x button at the upper-right corner to work
         onCancel={() => {
           setInputLx(foundation?.lx);
+          rejectRef.current = false;
           setWidthDialogVisible(false);
         }}
         destroyOnClose={false}
@@ -178,7 +306,9 @@ const FoundationWidthInput = ({
               onChange={(value) => setInputLx(value)}
               onPressEnter={(event) => {
                 setLx(inputLx);
-                setWidthDialogVisible(false);
+                if (!rejectRef.current) {
+                  setWidthDialogVisible(false);
+                }
               }}
             />
             <div style={{ paddingTop: '20px', textAlign: 'left', fontSize: '11px' }}>
@@ -196,9 +326,6 @@ const FoundationWidthInput = ({
             <Radio.Group onChange={onScopeChange} value={foundationActionScope}>
               <Space direction="vertical">
                 <Radio value={Scope.OnlyThisObject}>{i18n.t('foundationMenu.OnlyThisFoundation', lang)}</Radio>
-                <Radio value={Scope.AllConnectedObjects}>
-                  {i18n.t('foundationMenu.AllConnectedFoundations', lang)}
-                </Radio>
                 <Radio value={Scope.AllObjectsOfThisType}>{i18n.t('foundationMenu.AllFoundations', lang)}</Radio>
               </Space>
             </Radio.Group>
