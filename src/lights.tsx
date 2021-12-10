9bafe48b8a831f4b6afe9e56799662b3af024a62
Xiaotong <xd59@njit.edu>
2021-12-10 13:55:44 -0500

    fix shadow bug

--- src/lights.tsx
+++ src/lights.tsx
@@ -2,21 +2,33 @@
  * @Copyright 2021. Institute for Future Intelligence, Inc.
  */
 
-import React from 'react';
+import React, { useRef } from 'react';
 import { useStore } from './stores/common';
 import * as Selector from './stores/selector';
 import { DEFAULT_FAR } from './constants';
+import { DirectionalLight, Mesh } from 'three';
 
 const Lights = () => {
   const sunlightDirection = useStore(Selector.sunlightDirection);
   const sceneRadius = useStore(Selector.sceneRadius);
   const positionExtent = 2 * sceneRadius;
-  const cameraExtent = 10 * sceneRadius;
+  const cameraExtent = 2 * sceneRadius;
+
+  const ref = useRef<DirectionalLight>();
+
+  if (ref.current) {
+    ref.current.shadow.camera.left = -cameraExtent;
+    ref.current.shadow.camera.bottom = -cameraExtent;
+    ref.current.shadow.camera.right = cameraExtent;
+    ref.current.shadow.camera.top = cameraExtent;
+    ref.current.shadow.camera.updateProjectionMatrix();
+  }
 
   return (
     <>
       <ambientLight intensity={0.25} name={'Ambient Light'} />
       <directionalLight
+        ref={ref}
         name={'Directional Light'}
         color="white"
         position={sunlightDirection.normalize().multiplyScalar(positionExtent)}
@@ -26,10 +38,6 @@ const Lights = () => {
         shadow-mapSize-width={4096}
         shadow-camera-near={1}
         shadow-camera-far={DEFAULT_FAR}
-        shadow-camera-left={-cameraExtent}
-        shadow-camera-right={cameraExtent}
-        shadow-camera-top={cameraExtent}
-        shadow-camera-bottom={-cameraExtent}
       />
     </>
   );
