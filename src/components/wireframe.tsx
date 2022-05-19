90ee524b9435e98f3677dd94b7d6d8810fcdb6f4
Xiaotong <xd59@njit.edu>
2022-05-19 16:21:08 -0400

    wireframe

--- src/components/wireframe.tsx
+++ src/components/wireframe.tsx
@@ -5,8 +5,8 @@
  * when there is a ground image.
  */
 
-import React from 'react';
-import { Line } from '@react-three/drei';
+import React, { useEffect, useRef } from 'react';
+import { BufferGeometry, LineBasicMaterial, Mesh, Vector3 } from 'three';
 
 export interface WireframeProps {
   hx: number;
@@ -17,35 +17,61 @@ export interface WireframeProps {
 }
 
 const Wireframe = ({ hx, hy, hz, lineColor = 'black', lineWidth = 0.2 }: WireframeProps) => {
-  return (
-    <Line
-      points={[
-        [-hx, -hy, -hz],
-        [hx, -hy, -hz],
-        // draw vertical line between faces
-        [hx, -hy, hz],
-        [hx, -hy, -hz],
-        [hx, hy, -hz],
-        // draw vertical line between faces
-        [hx, hy, hz],
-        [hx, hy, -hz],
-        [-hx, hy, -hz],
-        // draw vertical line between faces
-        [-hx, hy, hz],
-        [-hx, hy, -hz],
-        [-hx, -hy, -hz],
-        [-hx, -hy, hz],
-        [hx, -hy, hz],
-        [hx, hy, hz],
-        [-hx, hy, hz],
-        [-hx, -hy, hz],
-      ]}
-      name={'Wireframe'}
-      userData={{ unintersectable: true }}
-      lineWidth={lineWidth}
-      color={lineColor}
-    />
-  );
+  const ref = useRef<Mesh>(null);
+
+  // Upper-Lower / Front-Back / Left-Right
+  const UFL = new Vector3(-hx, -hy, hz);
+  const UFR = new Vector3(hx, -hy, hz);
+  const UBL = new Vector3(-hx, hy, hz);
+  const UBR = new Vector3(hx, hy, hz);
+  const LFL = new Vector3(-hx, -hy, -hz);
+  const LFR = new Vector3(hx, -hy, -hz);
+  const LBL = new Vector3(-hx, hy, -hz);
+  const LBR = new Vector3(hx, hy, -hz);
+
+  useEffect(() => {
+    if (ref.current) {
+      const points = [
+        // upper
+        UFL,
+        UFR,
+        UFR,
+        UBR,
+        UBR,
+        UBL,
+        UBL,
+        UFL,
+
+        // lower
+        LFL,
+        LFR,
+        LFR,
+        LBR,
+        LBR,
+        LBL,
+        LBL,
+        LFL,
+
+        // vertical
+        UFL,
+        LFL,
+        UFR,
+        LFR,
+        UBR,
+        LBR,
+        UBL,
+        LBL,
+      ];
+
+      const geometry = new BufferGeometry().setFromPoints(points);
+      const material = new LineBasicMaterial({ color: lineColor, linewidth: lineWidth });
+
+      ref.current.geometry = geometry;
+      ref.current.material = material;
+    }
+  }, []);
+
+  return <lineSegments ref={ref} />;
 };
 
 export default React.memo(Wireframe);
