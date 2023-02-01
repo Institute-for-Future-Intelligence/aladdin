bb1437dbfafe173b48361ad3f9b6d30b9e967efb
Xiaotong <xd59@njit.edu>
2023-02-01 00:20:37 -0500

    fix empty wall bug

--- src/views/wall/emptyWall.tsx
+++ src/views/wall/emptyWall.tsx
@@ -4,15 +4,37 @@
 
 import { Plane } from '@react-three/drei';
 import { ThreeEvent } from '@react-three/fiber';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HALF_PI } from 'src/constants';
 import { WallModel } from 'src/models/WallModel';
 import { useStore } from 'src/stores/common';
 import { ActionType, ObjectType } from 'src/types';
 import { DoubleSide } from 'three';
+import * as Selector from 'src/stores/selector';
 
 const EmptyWall = (wallModel: WallModel) => {
-  const { lx, lz, parentId, id } = wallModel;
+  const { lx, lz, parentId, id, roofId } = wallModel;
+
+  const deletedRoofId = useStore(Selector.deletedRoofId);
+
+  useEffect(() => {
+    if (deletedRoofId === roofId) {
+      useStore.getState().set((state) => {
+        for (const e of state.elements) {
+          if (e.id === id && e.type === ObjectType.Wall) {
+            const wall = e as WallModel;
+            wall.roofId = null;
+            wall.leftRoofHeight = undefined;
+            wall.rightRoofHeight = undefined;
+            wall.centerRoofHeight = undefined;
+            wall.centerLeftRoofHeight = undefined;
+            wall.centerRightRoofHeight = undefined;
+            break;
+          }
+        }
+      });
+    }
+  }, [deletedRoofId]);
 
   const checkIfCanSelectMe = (e: ThreeEvent<PointerEvent>) => {
     return !(
