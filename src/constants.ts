c946319447f9dfef578166391ce93c5678e84e37
Charles Xie <charxie@gmail.com>
2023-02-08 12:55:04 -0500

    update AI examples

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@
 
 import { Color, Euler, Vector2, Vector3 } from 'three';
 
-export const VERSION = '0.7.8';
+export const VERSION = '0.7.9';
 
 export const isProd = process.env.NODE_ENV === 'production';
 
