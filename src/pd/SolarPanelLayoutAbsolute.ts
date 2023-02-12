8d8369517c72f6c4d8d5ea3ed1fd144dd2f5277a
Charles Xie <charxie@gmail.com>
2023-02-11 21:31:28 -0500

    for now, support both relative and absolute layout

--- src/pd/SolarPanelLayoutAbsolute.ts
+++ src/pd/SolarPanelLayoutAbsolute.ts
@@ -8,7 +8,7 @@ import { ObjectType, Orientation, RowAxis } from '../types';
 import { ElementModel } from '../models/ElementModel';
 import { Point2 } from '../models/Point2';
 import { ElementModelFactory } from '../models/ElementModelFactory';
-import { UNIT_VECTOR_POS_Z } from '../constants';
+import { HALF_PI, UNIT_VECTOR_POS_Z } from '../constants';
 import { PvModel } from '../models/PvModel';
 import { SolarPanelModel } from '../models/SolarPanelModel';
 
@@ -38,84 +38,111 @@ export class SolarPanelLayoutAbsolute {
     const bounds = Util.calculatePolygonBounds(verticesRotated);
     const ly = (orientation === Orientation.portrait ? pvModel.length : pvModel.width) * rowsPerRack;
     const h = 0.5 * Math.abs(Math.sin(tiltAngle)) * ly;
-    const n = Math.floor((bounds.maxY() - bounds.minY() - 2 * margin - ly) / interRowSpacing);
-    const start = bounds.minY() + ly / 2 + margin;
-    let a: Point2 = { x: bounds.minX(), y: 0 } as Point2;
-    let b: Point2 = { x: bounds.maxX(), y: 0 } as Point2;
-    for (let i = 0; i <= n; i++) {
-      const cy = start + i * interRowSpacing;
-      a.y = b.y = cy - h;
-      const p1 = Util.polygonIntersections(a, b, verticesRotated).sort((v1, v2) => v1.x - v2.x);
-      a.y = b.y = cy + h;
-      const p2 = Util.polygonIntersections(a, b, verticesRotated).sort((v1, v2) => v1.x - v2.x);
-      const numberOfSegments = Math.max(p1.length, p2.length) / 2;
-      if (numberOfSegments > 0) {
-        for (let s = 0; s < numberOfSegments; s++) {
-          const t = s * 2;
-          const f1 = p1[t] ?? p2[t];
-          const g1 = p1[t + 1] ?? p2[t + 1];
-          const f2 = p2[t] ?? p1[t];
-          const g2 = p2[t + 1] ?? p1[t + 1];
-          const shorter = Math.hypot(f1.x - g1.x, f1.y - g1.y) <= Math.hypot(f2.x - g2.x, f2.y - g2.y);
-          const x1 = shorter ? f1.x : f2.x;
-          const x2 = shorter ? g1.x : g2.x;
-          const y1 = shorter ? f1.y : f2.y;
-          const y2 = shorter ? g1.y : g2.y;
-          const lx = Math.hypot(x1 - x2, y1 - y2) - 2 * margin;
-          if (lx > 0) {
-            let cp = Util.relativeCoordinates((x1 + x2) / 2, cy, 0, base);
-            const panel = SolarPanelLayoutAbsolute.makeSegment(
-              cp.x,
-              cp.y,
-              lx,
-              ly,
-              0,
-              base,
-              pvModel,
-              tiltAngle,
-              poleHeight,
-              poleSpacing,
-              margin,
-            );
-            panel.referenceId = area.id;
-            Util.changeOrientation(panel, pvModel, orientation);
-            solarPanels.push(panel);
+    if (rowAxis === RowAxis.eastWest) {
+      const maxLength = bounds.maxY() - bounds.minY();
+      const n = Math.floor((maxLength - 2 * margin - ly) / interRowSpacing);
+      const start = bounds.minY() + ly / 2 + margin;
+      let a: Point2 = { x: bounds.minX(), y: 0 } as Point2;
+      let b: Point2 = { x: bounds.maxX(), y: 0 } as Point2;
+      for (let i = 0; i <= n; i++) {
+        const cy = start + i * interRowSpacing;
+        a.y = b.y = cy - h;
+        const p1 = Util.polygonIntersections(a, b, verticesRotated).sort((v1, v2) => v1.x - v2.x);
+        a.y = b.y = cy + h;
+        const p2 = Util.polygonIntersections(a, b, verticesRotated).sort((v1, v2) => v1.x - v2.x);
+        const numberOfSegments = Math.max(p1.length, p2.length) / 2;
+        if (numberOfSegments > 0) {
+          for (let s = 0; s < numberOfSegments; s++) {
+            const t = s * 2;
+            const f1 = p1[t] ?? p2[t];
+            const g1 = p1[t + 1] ?? p2[t + 1];
+            const f2 = p2[t] ?? p1[t];
+            const g2 = p2[t + 1] ?? p1[t + 1];
+            const shorter = Math.hypot(f1.x - g1.x, f1.y - g1.y) <= Math.hypot(f2.x - g2.x, f2.y - g2.y);
+            const x1 = shorter ? f1.x : f2.x;
+            const x2 = shorter ? g1.x : g2.x;
+            const y1 = shorter ? f1.y : f2.y;
+            const y2 = shorter ? g1.y : g2.y;
+            const lx = Math.hypot(x1 - x2, y1 - y2) - 2 * margin;
+            if (lx > 0) {
+              let cp = Util.relativeCoordinates((x1 + x2) / 2, cy, 0, base);
+              const panel = ElementModelFactory.makeSolarPanel(
+                base,
+                pvModel,
+                cp.x,
+                cp.y,
+                base.lz,
+                Orientation.portrait,
+                poleHeight,
+                poleSpacing,
+                tiltAngle,
+                -base.rotation[2],
+                UNIT_VECTOR_POS_Z,
+                base.rotation,
+                undefined,
+                lx,
+                ly,
+              );
+              panel.referenceId = area.id;
+              Util.changeOrientation(panel, pvModel, orientation);
+              solarPanels.push(panel);
+            }
+          }
+        }
+      }
+    } else if (rowAxis === RowAxis.northSouth) {
+      const maxLength = bounds.maxX() - bounds.minX();
+      const n = Math.floor((maxLength - 2 * margin - ly) / interRowSpacing);
+      const start = bounds.minX() + ly / 2 + margin;
+      let a: Point2 = { x: 0, y: bounds.minY() } as Point2;
+      let b: Point2 = { x: 0, y: bounds.maxY() } as Point2;
+      for (let i = 0; i <= n; i++) {
+        const cx = start + i * interRowSpacing;
+        a.x = b.x = cx - h;
+        const p1 = Util.polygonIntersections(a, b, verticesRotated).sort((v1, v2) => v1.y - v2.y);
+        a.x = b.x = cx + h;
+        const p2 = Util.polygonIntersections(a, b, verticesRotated).sort((v1, v2) => v1.y - v2.y);
+        const numberOfSegments = Math.max(p1.length, p2.length) / 2;
+        if (numberOfSegments > 0) {
+          for (let s = 0; s < numberOfSegments; s++) {
+            const t = s * 2;
+            const f1 = p1[t] ?? p2[t];
+            const g1 = p1[t + 1] ?? p2[t + 1];
+            const f2 = p2[t] ?? p1[t];
+            const g2 = p2[t + 1] ?? p1[t + 1];
+            const shorter = Math.hypot(f1.x - g1.x, f1.y - g1.y) <= Math.hypot(f2.x - g2.x, f2.y - g2.y);
+            const x1 = shorter ? f1.x : f2.x;
+            const x2 = shorter ? g1.x : g2.x;
+            const y1 = shorter ? f1.y : f2.y;
+            const y2 = shorter ? g1.y : g2.y;
+            const lx = Math.hypot(x1 - x2, y1 - y2) - 2 * margin;
+            if (lx > 0) {
+              let cp = Util.relativeCoordinates(cx, (y1 + y2) / 2, 0, base);
+              const panel = ElementModelFactory.makeSolarPanel(
+                base,
+                pvModel,
+                cp.x,
+                cp.y,
+                base.lz,
+                Orientation.portrait,
+                poleHeight,
+                poleSpacing,
+                tiltAngle,
+                -base.rotation[2] + HALF_PI,
+                UNIT_VECTOR_POS_Z,
+                base.rotation,
+                undefined,
+                lx,
+                ly,
+              );
+              panel.referenceId = area.id;
+              Util.changeOrientation(panel, pvModel, orientation);
+              solarPanels.push(panel);
+            }
           }
         }
       }
     }
     return solarPanels;
-  }
-
-  static makeSegment(
-    cx: number,
-    cy: number,
-    lx: number,
-    ly: number,
-    relativeAzimuth: number,
-    base: ElementModel,
-    pvModel: PvModel,
-    tiltAngle: number,
-    poleHeight: number,
-    poleSpacing: number,
-    margin: number,
-  ) {
-    return ElementModelFactory.makeSolarPanel(
-      base,
-      pvModel,
-      cx,
-      cy,
-      base.lz,
-      Orientation.portrait,
-      poleHeight,
-      poleSpacing,
-      tiltAngle,
-      -base.rotation[2],
-      UNIT_VECTOR_POS_Z,
-      base.rotation,
-      undefined,
-      lx,
-      ly,
-    );
   }
 }
